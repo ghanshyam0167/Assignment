8887daed4ac7fe069c427db843c444707a5045a7
ghanshyam0167 <ghanshyam0167.be23@chitkara.edu.in>
2025-07-30 11:18:52 +0530

    Adding About me Project

---
+++ AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by GS Agrawal on 30/07/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
